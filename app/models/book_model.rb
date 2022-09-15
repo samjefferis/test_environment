485162f740f184ec794fdcae1c3741aeb7b578b2
root <root@localhost>
2022-09-15 19:52:14 +0000

    Updated files

---
+++ app/models/book_model.rb
@@ -0,0 +1,2 @@
+class BookModel < ApplicationRecord
+end
--- spec/unit/unit_spec.rb
+++ spec/unit/unit_spec.rb
@@ -2,8 +2,9 @@
 require 'rails_helper'
 
 RSpec.describe Book, type: :model do
+  #Title
   subject do
-    described_class.new(title: 'harry potter')
+    described_class.new(title: 'harry potter', author: 'JK Rowling', price: '15', published: Date.today)
   end
 
   it 'is valid with valid attributes' do
@@ -14,4 +15,24 @@ RSpec.describe Book, type: :model do
     subject.title = nil
     expect(subject).not_to be_valid
   end
+
+  #Author
+  it 'is not valid without an author' do
+    subject.author = nil
+    expect(subject).not_to be_valid
+  end
+
+  #Price
+  it 'is not valid without a price' do
+    subject.price = nil
+    expect(subject).not_to be_valid
+  end
+
+  #Published Date
+  it 'is not valid without a date' do
+    subject.published = nil
+    expect(subject).not_to be_valid
+  end
+
 end
+
